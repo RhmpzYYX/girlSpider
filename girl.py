bee67ebbf92214e9e9feff2cd190cd8b3c9cd890
RhmpzYYX <yangyuxuan005@126.com>
2021-03-21 23:25:01 +0800

    2
    2

--- girl.py
+++ girl.py
@@ -6,7 +6,7 @@
 # @File    : girl.py
 # @Software: PyCharm
 # @Describe:小姐姐网页爬取
-
+import time
 import requests
 import re
 url="https://www.mn53.com/meihuoxiezhen/"
@@ -16,8 +16,12 @@ results=re.findall(pattern,res.text)
 
 num=1
 for img_url in results:
+    time.sleep(0.5)
     response=requests.get("https:"+img_url)
     with open('img/%d.jpg'%num,'wb') as f:
         f.write(response.content)
     print("已经下载%d张："%num+"https:"+img_url)
     num=num+1
+
+#todo
+# urllib3.exceptions.ProtocolError: ('Connection aborted.', ConnectionResetError(10054, '远程主机强迫关闭了一个现有的连接。', None, 10054, None))
